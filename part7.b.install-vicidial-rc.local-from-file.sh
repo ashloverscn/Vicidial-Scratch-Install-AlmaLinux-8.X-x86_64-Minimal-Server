673efdb862d27116a8fd8d6423a538a69ecfcbe9
ashloverscn <ashloverscn@gmail.com>
2026-05-07 12:47:33 +0530

    Update part7.b.install-vicidial-rc.local-from-file.sh

--- part7.b.install-vicidial-rc.local-from-file.sh
+++ part7.b.install-vicidial-rc.local-from-file.sh
@@ -4,7 +4,7 @@ echo -e "\e[0;32m Install rc.local entry for vicidial services startup \e[0m"
 sleep 2
 cd /usr/src
 \cp -r /etc/rc.d/rc.local /etc/rc.d/rc.local.original 
-#wget -O /usr/src/rc.local https://github.com/ashloverscn/Vicidial-Scratch-Install-AlmaLinux-8.8-x86_64-Minimal-Server/raw/main/rc.local
+#wget -O /usr/src/rc.local https://github.com/ashloverscn/Vicidial-Scratch-Install-AlmaLinux-9.X-x86_64-Minimal-Server/raw/main/rc.local
 \cp -r /usr/src/rc.local /etc/rc.d/rc.local
 
 ##disable Script early exit
@@ -12,7 +12,8 @@ sudo sed -i 's|exit 0|### exit 0|g' /etc/rc.d/rc.local
 
 chmod +x /etc/rc.d/rc.local
 systemctl enable rc-local
-systemctl start rc-local
+#systemctl start rc-local &
+systemctl restart rc-local &
 
 echo -e "\e[0;32m add rc-local as a Service for vicidial ServiceS Startup \e[0m"
 sleep 2
@@ -31,5 +32,3 @@ RemainAfterExit=yes
 [Install]
 WantedBy=multi-user.target
 EOF
-
-
